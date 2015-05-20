975a7255ef9b72a4d41edf45984ebf1fd397eb9f
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-05-20 12:27:31 +0200

    initial commit

---
+++ aREST_UI.h
@@ -0,0 +1,154 @@
+/* 
+  aREST UI for Arduino & the ESP8266
+  See the README file for more details.
+ 
+  Written in 2015 by Marco Schwartz under a GPL license. 
+  Version 1.0.0
+  Changelog:
+  
+  Version 1.0.0: Initial release with buttons only
+*/
+
+#ifndef aRest_ui_h
+#define aRest_ui_h
+
+// Include Arduino header
+#include "Arduino.h"
+
+// Using ESP8266 ?
+#if defined(ESP8266)
+#include "stdlib_noniso.h"
+#endif
+
+class aREST_UI: public aREST {
+
+public:
+
+aREST_UI() {
+
+}
+
+// Create button
+void button(int pin){
+
+  // Set pin as output
+  pinMode(pin,OUTPUT);
+
+  // Set in button array
+  buttons[buttons_index] = pin;
+  buttons_index++;
+
+}
+
+// void slider(int pin) {
+
+//   // Set pin as output
+//   pinMode(pin,OUTPUT);
+
+//   // Set in button array
+//   sliders[sliders_index] = pin;
+//   sliders_index++;
+
+// }
+
+// Create indicator
+// void indicator(char * variable){
+
+//   indicators[0] = variable;
+
+// }
+
+// Handle connection
+virtual void root_answer() {
+        
+    // Answer
+    addToBuffer("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
+    addToBuffer("<html><head>");
+    addToBuffer("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+    addToBuffer("<script ");
+    addToBuffer("src=\"http://code.jquery.com/jquery-2.1.3.min.js\">");
+    addToBuffer("</script>");
+    addToBuffer("<script type='text/javascript' src='http://cdn.rawgit.com/Foliotek/AjaxQ/master/ajaxq.js'></script>");
+    addToBuffer("<style>.row {margin-top: 30px;} .indicator {font-size: 30px; vertical-align: middle;}</style>");
+    addToBuffer("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css\">");
+    addToBuffer("</head><body>");
+    addToBuffer("<div class=\"container\">");
+    addToBuffer("<h1>Relay Control</h1>");
+
+    // Buttons UI
+    for (int i = 0; i < buttons_index; i++) {
+      addToBuffer("<div class=\"row\">");
+      addToBuffer("<div class=\"col-md-2\"><button class=\"btn btn-block btn-lg btn-primary\" id='btn_on");
+      addToBuffer(buttons[i]);
+      addToBuffer("'>On</button></div>");
+      addToBuffer("<div class=\"col-md-2\"><button class=\"btn btn-block btn-lg btn-danger\" id='btn_off");
+      addToBuffer(buttons[i]);
+      addToBuffer("'>Off</button></div>");
+      addToBuffer("</div>");
+    }
+
+    // // Sliders UI
+    // for (int i = 0; i < sliders_index; i++) {
+    //   addToBuffer("<div class=\"row\">");
+    //   addToBuffer("<div class=\"col-md-2\"><input type='range' value='0' max='255' min='0' step='5' id='slider");
+    //   addToBuffer(sliders[i]);
+    //   addToBuffer("'></div>");
+    //   addToBuffer("</div>");
+    // }
+    
+    addToBuffer("<div class=\"row\">");
+    addToBuffer("<div class='col-md-4 indicator' id='temperature'></div>");
+    addToBuffer("</div>");
+
+    addToBuffer("</div>");
+
+    addToBuffer("<script>$( document ).ready(function() {");
+
+    // Buttons JavaScript
+    for (int i = 0; i < buttons_index; i++) {
+      addToBuffer("$('#btn_on");
+      addToBuffer(buttons[i]);
+      addToBuffer("').click(function() {$.get('/digital/");
+      addToBuffer(buttons[i]);
+      addToBuffer("/1');});");
+      addToBuffer("$('#btn_off");
+      addToBuffer(buttons[i]);
+      addToBuffer("').click(function() {$.get('/digital/");
+      addToBuffer(buttons[i]);
+      addToBuffer("/0');});");    
+    }
+
+    // Sliders JavaScript
+    // for (int i = 0; i < sliders_index; i++) {
+    //   addToBuffer("$('#slider");
+    //   addToBuffer(sliders[i]);
+    //   addToBuffer("').mouseup(function() {var val = $('#slider");
+    //   addToBuffer(sliders[i]);
+    //   addToBuffer("').val(); $.get('/analog/");
+    //   addToBuffer(sliders[i]);
+    //   addToBuffer("/' + val); });");      
+    // }
+
+    // addToBuffer("$.get('/temperature', function(data) { $('#temperature').html('Temperature: ' + data.temperature); });");
+    // addToBuffer("});</script>");
+
+    addToBuffer("</body></html>\r\n");
+
+}
+
+private:
+
+  // Buttons array
+  int buttons[10];
+  int buttons_index;
+
+  // Buttons array
+  int sliders[10];
+  int sliders_index;
+  
+  // Indicators array
+  char * indicators[10];
+
+};
+
+#endif
---
+++ examples/ESP8266/ESP8266.ino
@@ -0,0 +1,92 @@
+/* 
+  This a simple example of the aREST UI Library for the ESP8266.
+  See the README file for more details.
+ 
+  Written in 2014 by Marco Schwartz under a GPL license. 
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <aREST.h>
+#include <aREST_UI.h>
+
+// Create aREST instance
+aREST_UI rest = aREST_UI();
+
+// WiFi parameters
+const char* ssid = "yourSSID";
+const char* password = "yourPassword";
+
+// The port to listen for incoming TCP connections 
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int temperature;
+float humidity;
+
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(115200);
+  
+  // Create button to control pin 5
+  rest.button(5);
+  
+  // Init variables and expose them to REST API
+  temperature = 22;
+  humidity = 39.1;
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+
+  // Function to be exposed
+  rest.function("led",ledControl);
+    
+  // Give name and ID to device
+  rest.set_id("1");
+  rest.set_name("esp8266");
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+ 
+}
+
+int ledControl(String command) {
+  
+  Serial.println(command);
+  
+  // Get state from command
+  int state = command.toInt();
+ 
+  digitalWrite(5,state);
+  return 1;
+}
